c933813d14e5f928eeffc0b2e64111ecd338e7fa
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-11 17:33:24 -0300

    devCommit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto:wght@400;500;700;900&family=Sulphur+Point:wght@300;400;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main/>
+          <NextScript/>
+        </body>
+      </Html>
+    )
+  }
+}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,24 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  colors: {
+    gray: {
+      "900": "#181823",
+      "800": "#1F2829",
+      "700": "#353646",
+      "600": "#4B4D63",
+      "500": "#616488",
+      "400": "#797D9A",
+      "300": "#9699BB",
+      "200": "#B3B5C6",
+      "100": "#D1D2D0",
+      "50": "#EEEEF2",
+    }
+  },
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+  },
   styles: {
     global: {
       body: {
