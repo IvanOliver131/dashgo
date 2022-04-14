1de3313049d226cd8dbc3f3007c12d0466673185
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-14 17:22:13 -0300

    devCommit

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,7 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
@@ -21,10 +23,107 @@ export default function UserList() {
           bg="gray.800"
           p="8"
         >
-          
+          <Flex
+            mb="8"
+            justifyContent="space-between"
+            alignContent="center"
+          >
+            <Heading
+              size="lg"
+              fontWeight="normal"
+            >
+              Usuários
+            </Heading>
+            <Button 
+              as="a" 
+              size="sm" 
+              fontSize="small"
+              colorScheme="pink"
+              cursor="pointer"
+              leftIcon={<Icon as={RiAddLine}  fontSize="20"/>}
+            >
+              Criar novo
+            </Button>
+          </Flex> 
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th
+                  px="6"
+                  color="gray.300"
+                  width="8"
+                >
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>
+                  Usuário
+                </Th>
+                <Th>
+                  Data de cadastro
+                </Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Ivan Oliveira</Text>
+                    <Text fontSize="sm" color="gray.300">ivanoliver131@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  14 de Abril, 2022
+                </Td>
+                <Td>
+                  <Button 
+                    as="a" 
+                    size="sm" 
+                    fontSize="small"
+                    colorScheme="purple"
+                    cursor="pointer"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Ana Oliveira</Text>
+                    <Text fontSize="sm" color="gray.300">anaoliveira@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  14 de Abril, 2022
+                </Td>
+                <Td>
+                  <Button 
+                    as="a" 
+                    size="sm" 
+                    fontSize="small"
+                    colorScheme="purple"
+                    cursor="pointer"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
-
   );
 }
