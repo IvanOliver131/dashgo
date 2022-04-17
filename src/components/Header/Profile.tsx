6c1a777cbea67078854c1f104753d1df654d38a1
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-17 18:26:43 -0300

    devCommit

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return(
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>Ivan Oliveira</Text>
-        <Text 
-          color="gray.300" 
-          fontSize="small"
-        >
-          ivanoliver131@gmail.com
-        </Text>
-      </Box>
-
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Ivan Oliveira</Text>
+          <Text 
+            color="gray.300" 
+            fontSize="small"
+          >
+            ivanoliver131@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar 
         size="md"
         name="Ivan Oliveira"
