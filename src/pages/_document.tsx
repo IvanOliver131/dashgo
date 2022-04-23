25f6052fe7a9435607ba2270226c98ba9fc6a774
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-04-23 15:20:13 -0300

    devCommit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-// Escopo basico para o indexHtml
+// Escopo básico para o indexHtml
 export default class MyDocument extends Document {
   render() {
     return (
@@ -10,8 +10,8 @@ export default class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Roboto:wght@400;500;700;900&family=Sulphur+Point:wght@300;400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
-          <Main/>
-          <NextScript/>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     )
